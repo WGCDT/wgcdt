f78c2daee63e2e192f8575ee45d8e7c3f1498d91
harfordt <tim.harford@wgc.school.nz>
2015-07-25 17:19:53 +1200

    Styled gallery thumbnails and sped up lightbox animations

--- js/slimbox2.js
+++ js/slimbox2.js
@@ -8,7 +8,11 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
 	jQuery(function($) {
-		$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
+		$("a[rel^='lightbox']").slimbox({
+                                        captionAnimationDuration:1,
+                                        overlayFadeDuration:200,
+                                        imageFadeDuration:200,
+                                        loop:true}, null, function(el) {
 			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 		});
 	});
